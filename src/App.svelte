b3931a924f4b52223cf001f7d41677a72979e9a6
Paul <paul21479@gmail.com>
2023-06-23 23:08:50 +1200

    Added Routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+  import { Router, Link, Route } from "svelte-routing";
   import Home from "./layout/Home.svelte";
+  import Settings from "./layout/Settings.svelte";
+
+  export let url = "/";
 </script>
 
 <svelte:head>
   <title>Home - FuserSM</title>
 </svelte:head>
 
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/settings">Settings</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
 <Home />
--- src/layout/Home.svelte
+++ src/layout/Home.svelte
@@ -1,4 +1,4 @@
 <main>
-  <h1>Welcome to FusiomSM</h1>
+  <h1>Welcome to FuserSM</h1>
   <p>Here is the tester!!!</p>
 </main>
